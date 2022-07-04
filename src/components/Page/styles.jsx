16e814d903bb47d3d8b24c3e3422746dfa8e8668
H-459 <leshem.guy@gmail.com>
2022-07-04 16:41:22 +0300

    started styling

---
+++ src/components/Page/styles.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const productFlexWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    max-width:70%;
+   
+`;
+
+export const productFlexItem = styled.div`
+    border: 1px solid green;
+   width: 15%;
+  margin-left: 24px;
+  margin-bottom:48px;
+`;
+
